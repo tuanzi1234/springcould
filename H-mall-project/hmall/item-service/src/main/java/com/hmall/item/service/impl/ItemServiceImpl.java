aea3e3f5b1743050e144f1bb6c1c0131613a1876
tuanzi <lvbohan2003@163.com>
2025-09-26 04:47:28 +0800

    分布式事务完结

--- H-mall-project/hmall/item-service/src/main/java/com/hmall/item/service/impl/ItemServiceImpl.java
+++ H-mall-project/hmall/item-service/src/main/java/com/hmall/item/service/impl/ItemServiceImpl.java
@@ -9,6 +9,7 @@ import com.hmall.item.domain.po.Item;
 import com.hmall.item.mapper.ItemMapper;
 import com.hmall.item.service.IItemService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.List;
@@ -23,6 +24,7 @@ import java.util.List;
 @Service
 public class ItemServiceImpl extends ServiceImpl<ItemMapper, Item> implements IItemService {
 
+    @Transactional
     @Override
     public void deductStock(List<OrderDetailDTO> items) {
         String sqlStatement = "com.hmall.item.mapper.ItemMapper.updateStock";
