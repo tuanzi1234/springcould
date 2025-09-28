9c67c2e242eb522ed583597873c3ea75b7dca6d0
tuanzi <lvbohan2003@163.com>
2025-09-29 04:56:30 +0800

    MQ完结

--- H-mall-project/hmall/trade-service/src/main/java/com/hmall/trade/listener/PayStatusListener.java
+++ H-mall-project/hmall/trade-service/src/main/java/com/hmall/trade/listener/PayStatusListener.java
@@ -1,5 +1,6 @@
 package com.hmall.trade.listener;
 
+import com.hmall.trade.domain.po.Order;
 import com.hmall.trade.service.IOrderService;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
@@ -20,6 +21,14 @@ public class PayStatusListener {
             key = "pay.success"
     ))
     public void ListenPaySuccess(Long orderId) {
+        // 实现业务幂等性
+        // 1. 查询订单
+        Order order = orderService.getById(orderId);
+        // 2. 判断订单是否支付成功
+        if (order == null || order.getStatus() != 1) {
+            // 未支付则不做处理
+            return;
+        }
         orderService.markOrderPaySuccess(orderId);
     }
 }
